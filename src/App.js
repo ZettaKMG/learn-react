6b9518daaa1296d561d30d292ade8f49800496be
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-04 14:01:25 +0900

    fragment 표현 간소화

--- src/App.js
+++ src/App.js
@@ -13,14 +13,28 @@
 // export default App;
 
 // div 요소 사용하고 싶지 않을 때, Fragment 기능 사용 가능(리액트 v16 이상부터)
-import React, { Fragment } from 'react';
+// import React, { Fragment } from 'react';
+
+// function App() {
+//   return (
+//     <Fragment>
+//       <h1>리액트 안녕!</h1>
+//       <h2>잘 작동하니?</h2>
+//     </Fragment>
+//   );
+// }
+
+// export default App;
+
+// Fragment를 다음과 같은 형태로도 표현 가능
+import React from 'react';
 
 function App() {
   return (
-    <Fragment>
+    <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-    </Fragment>
+    </>
   );
 }
 
