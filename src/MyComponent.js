23284c4a7d70101a5f1f4e0328fdbd8adbe6ad13
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-05 15:50:59 +0900

    클래스형 컴포넌트에서 props 사용하기(class 내부에서 지정)

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -137,11 +137,49 @@
 
 // export default MyComponent;
 
-// 클래스형 컴포넌트에서 props 사용하기
+// 클래스형 컴포넌트에서 props 사용하기(render 함수에서 조회하는 형태)
+// import React, { Component } from 'react';
+// import propTypes from 'prop-types';
+
+// class MyComponent extends Component {
+//   render() {
+//     const { name, favoriteNumber, children } = this.props; // 비구조화 할당
+//     return (
+//       <div>
+//         안녕하세요, 제 이름은 {name}입니다. <br />
+//         children 값은 {children}
+//         입니다.
+//         <br />
+//         제가 좋아하는 숫자는 {favoriteNumber}입니다.
+//       </div>
+//     );
+//   }
+// }
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
+
+// MyComponent.propTypes = {
+//   name: propTypes.string,
+//   favoriteNumber: propTypes.number.isRequired,
+// };
+
+// export default MyComponent;
+
+// 클래스형 컴포넌트에서 props 사용하기(class 내부에서 지정)
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름',
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
+
   render() {
     const { name, favoriteNumber, children } = this.props; // 비구조화 할당
     return (
@@ -155,14 +193,5 @@ class MyComponent extends Component {
     );
   }
 }
-
-MyComponent.defaultProps = {
-  name: '기본 이름',
-};
-
-MyComponent.propTypes = {
-  name: propTypes.string,
-  favoriteNumber: propTypes.number.isRequired,
-};
 
 export default MyComponent;
