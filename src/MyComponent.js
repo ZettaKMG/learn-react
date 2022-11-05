e27a1ee033449df590c1ab1e8cbfe4636a264402
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-05 14:23:04 +0900

    비구조화 할당 문법2

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -48,14 +48,33 @@
 // export default MyComponent;
 
 // 비구조화 할당 문법
+// import React from 'react';
+
+// const MyComponent = (props) => {
+//   const { name, children } = props;
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {name}입니다. <br />
+//       children 값은 {children}
+//       입니다.
+//     </div>
+//   );
+// };
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
+
+// export default MyComponent;
+
+// 비구조화 할당 문법2
 import React from 'react';
 
-const MyComponent = (props) => {
-  const { name, children } = props;
+const MyComponent = ({ name, children }) => {
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}
       입니다.
     </div>
   );
