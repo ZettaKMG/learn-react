d791894794576399b570fe963d6741907e8f7397
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-05 15:06:01 +0900

    isRequired 사용

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -87,15 +87,41 @@
 // export default MyComponent;
 
 // propTypes로 props 검증
+// import React from 'react';
+// import propTypes from 'prop-types';
+
+// const MyComponent = ({ name, children }) => {
+//   return (
+//     <div>
+//       안녕하세요, 제 이름은 {name}입니다. <br />
+//       children 값은 {children}
+//       입니다.
+//     </div>
+//   );
+// };
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름'
+// };
+
+// MyComponent.propTypes = {
+//   name: propTypes.string
+// };
+
+// export default MyComponent;
+
+// isRequired를 사용하여 필수 propTypes 설정
 import React from 'react';
 import propTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다. <br />
       children 값은 {children}
       입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
@@ -106,6 +132,7 @@ MyComponent.defaultProps = {
 
 MyComponent.propTypes = {
   name: propTypes.string,
+  favoriteNumber: propTypes.number.isRequired,
 };
 
 export default MyComponent;
