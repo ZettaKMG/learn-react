a54de53b8e00a6109eb1d4845e38a5ea4ec0057e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-05 13:54:35 +0900

    컴포넌트 사용시 props 값 지정

--- src/App.js
+++ src/App.js
@@ -298,4 +298,12 @@
 
 // export default App;
 
-//
+// 컴포넌트를 사용할 때, props 값 지정하기
+import React from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+  return <MyComponent name="React" />;
+};
+
+export default App;
