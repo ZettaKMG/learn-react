0cb066c840b1db7d60583f0a2ffe1620acbbb3af
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-05 16:21:06 +0900

    this.setState에 함수인자 전달

--- src/Counter.js
+++ src/Counter.js
@@ -64,6 +64,36 @@
 // export default Counter;
 
 // state를 constructor에서 꺼내기
+// import React, { Component } from 'react';
+
+// class Counter extends Component {
+//   state = {
+//     number: 0,
+//     fixedNumber: 0
+//   };
+//   render() {
+//     const { number, fixedNumber } = this.state; // state를 조회할 때 this.state로 조회함.
+//     return (
+//       <div>
+//         <h1>{number}</h1>
+//         <h2>바뀌지 않는 값: {fixedNumber}</h2>
+//         <button
+//           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정.
+//           onClick={() => {
+//             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있음.
+//             this.setState({ number: number + 1 });
+//           }}
+//         >
+//           +1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
+// this.setState에 함수인자 전달
 import React, { Component } from 'react';
 
 class Counter extends Component {
@@ -81,7 +111,16 @@ class Counter extends Component {
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정.
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있음.
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            // 위 코드와 아래 코드는 완전히 똑같은 기능.
+            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
           }}
         >
           +1
