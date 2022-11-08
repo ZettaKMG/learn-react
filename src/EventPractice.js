842f4806a8751ef903ece3fa05e5ac2f3cd76106
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-08 22:45:30 +0900

    컴포넌트 생성

---
+++ src/EventPractice.js
@@ -0,0 +1,16 @@
+// 예제로 이벤트 핸들링 익히기
+// 순서 : 컴포넌트 생성 및 불러오기 > onChange 이벤트 핸들링하기 > 임의 메서드 만들기 > input 여러 개 다루기 > onKeyPress 이벤트 핸들링하기
+// 컴포넌트 생성 및 불러오기
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <>
+        <h1>이벤트 연습</h1>
+      </>
+    );
+  }
+}
+
+export default EventPractice;
