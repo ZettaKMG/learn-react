cbfc8bd83fa3d97200f6a67a1e7b8edc1f7c3d94
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-08 22:57:55 +0900

    SyntheticEvent 설명

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,22 @@
 // 예제로 이벤트 핸들링 익히기
 // 순서 : 컴포넌트 생성 및 불러오기 > onChange 이벤트 핸들링하기 > 임의 메서드 만들기 > input 여러 개 다루기 > onKeyPress 이벤트 핸들링하기
 // 컴포넌트 생성 및 불러오기
+// import React, { Component } from 'react';
+
+// class EventPractice extends Component {
+//   render() {
+//     return (
+//       <>
+//         <h1>이벤트 연습</h1>
+//       </>
+//     );
+//   }
+// }
+
+// export default EventPractice;
+
+// onChange 이벤트 핸들링하기
+// onChange 이벤트 설정
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
@@ -8,6 +24,14 @@ class EventPractice extends Component {
     return (
       <>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
       </>
     );
   }
