69bba2046912ed613509e22406203a758ec9e8ad
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-10 22:29:33 +0900

    데이터 추가기능 구현하기
    onClick 함수에서 배열의 내장 함수 concat을 활용, 새로운 항목을 추가한 배열을 만들고 setName를 통해 상태 업데이트

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -64,6 +64,34 @@
 // export default IterationSample;
 
 // 데이터 추가기능 구현하기
+// ul 태그 상단에 input과 button 렌더링 후, input 상태 관리
+// import React, {useState} from 'react';
+
+// const IterationSample = () => {
+//     const [names, setNames] = useState([
+//         {id: 1, text: '눈사람'},
+//         {id: 2, text: '얼음'},
+//         {id: 3, text: '눈'},
+//         {id: 4, text: '바람'}
+//     ]);
+//     const [inputText, setInputText] = useState('');
+//     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+//     const onChange = e => setInputText(e.target.value);
+
+//     const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+//     return (
+//         <>
+//             <input value={inputText} onChange={onChange} />
+//             <button>추가</button>
+//             <ul>{namesList}</ul>
+//         </>
+//     );    
+// };
+
+// export default IterationSample;
+
+// onClick 함수에서 배열의 내장 함수 concat을 활용, 새로운 항목을 추가한 배열을 만들고 setName를 통해 상태 업데이트
 import React, {useState} from 'react';
 
 const IterationSample = () => {
@@ -77,12 +105,21 @@ const IterationSample = () => {
     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
 
     const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId, // nextId 값을 id로 설정하고
+            text: inputText
+        });
+        setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
+        setNames(nextNames); // names 값을 업데이트한다.
+        setInputText(''); // inputText를 비운다.
+    };
 
     const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
     return (
         <>
             <input value={inputText} onChange={onChange} />
-            <button>추가</button>
+            <button onClick={onClick}>추가</button>
             <ul>{namesList}</ul>
         </>
     );    
