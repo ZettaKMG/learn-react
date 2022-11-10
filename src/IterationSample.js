60818f7ad13558f2039a0ff83c59e8afb8d9d0b8
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-10 22:15:27 +0900

    데이터 배열을 컴포넌트 배열로 변환하기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,15 +1,27 @@
 // 6장 컴포넌트 반복 시작
+// import React from 'react';
+
+// const IterationSample = () => {
+//     return (
+//         <ul>
+//             <li>눈사람</li>
+//             <li>얼음</li>
+//             <li>눈</li>
+//             <li>바람</li>
+//         </ul>
+//     );
+// };
+
+// export default IterationSample;
+
+// 데이터 배열을 컴포넌트 배열로 변환하기
+// 컴포넌트 수정하기
 import React from 'react';
 
 const IterationSample = () => {
-    return (
-        <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-        </ul>
-    );
+   const names = ['눈사람', '얼음', '눈', '바람'];
+   const nameList = names.map(name => <li>{name}</li>);
+   return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
