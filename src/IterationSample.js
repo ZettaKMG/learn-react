5234acf037cf3b04c11a00c13b0bed599a88f54d
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-10 22:17:02 +0900

    key 설정
    콘솔 경고 메세지 사라짐

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -16,11 +16,26 @@
 
 // 데이터 배열을 컴포넌트 배열로 변환하기
 // 컴포넌트 수정하기
+// import React from 'react';
+
+// const IterationSample = () => {
+//    const names = ['눈사람', '얼음', '눈', '바람'];
+//    const nameList = names.map(name => <li>{name}</li>);
+//    return <ul>{nameList}</ul>;
+// };
+
+// export default IterationSample;
+
+// key 설정
+// map 함수의 인자로 전달되는 함수 내부에서 컴포넌트 props를 설정하듯이 설정하면 됨
+// key 값은 언제나 유일해야 함(데이터의 고유값을 key값으로 설정)
+// ㄴ 고유값이 없을시에는 map 함수에 전달되는 콜백 함수 인수인 index 값 이용
+// 예제 컴포넌트 수정
 import React from 'react';
 
 const IterationSample = () => {
    const names = ['눈사람', '얼음', '눈', '바람'];
-   const nameList = names.map(name => <li>{name}</li>);
+   const nameList = names.map((name, index) => <li key={index}>{name}</li>);
    return <ul>{nameList}</ul>;
 };
 
