af6b271dd4214bf105e43225a0022c9d8f2dd69b
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-10 21:58:14 +0900

    컴포넌트 반복 예시

---
+++ src/IterationSample.js
@@ -0,0 +1,15 @@
+// 6장 컴포넌트 반복 시작
+import React from 'react';
+
+const IterationSample = () => {
+    return (
+        <ul>
+            <li>눈사람</li>
+            <li>얼음</li>
+            <li>눈</li>
+            <li>바람</li>
+        </ul>
+    );
+};
+
+export default IterationSample;
