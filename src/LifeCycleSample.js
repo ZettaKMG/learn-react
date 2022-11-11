d4ec99863e12cf670b90fb336064ff887d3bc963
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-11 18:09:44 +0900

    에러 잡아내기(의도적으로 render 함수에서 에러 발생시키기)

--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -98,6 +98,88 @@
 // LifeCycleSample 컴포넌트 만들기 > App에 렌더링하기 > 버튼 누르고 콘솔 창 관찰하기
 
 // 예제 컴포넌트 생성
+// import React, {Component} from 'react';
+
+// class LifeCycleSample extends Component {
+//     state = {
+//         number: 0,
+//         color: null,
+//     }
+
+//     myRef = null; // ref를 설정할 부분
+
+//     constructor(props) {
+//         super(props);
+//         console.log('constructor');
+//     }
+
+//     static getDerivedStateFromProps(nextProps, prevState) {
+//         console.log('getDerivedStateFromProps');
+//         if (nextProps.color !== prevState.color) {
+//             return {color: nextProps.color};
+//         }
+//         return null;
+//     }
+
+//     componentDidMount() {
+//         console.log('componentDidMount');
+//     }
+
+//     shouldComponentUpdate(nextProps, nextState) {
+//         console.log('shouldComponentUpdate', nextProps, nextState);
+//         // 숫자의 마지막 자리가 4면 리렌더링 하지 않음.
+//         return nextState.number % 10 !== 4;
+//     }
+
+//     componentWillUnmount() {
+//         console.log('componentWillUnmount');
+//     }
+
+//     handleClick = () => {
+//         this.setState({
+//             number: this.state.number + 1
+//         });
+//     }
+
+//     getSnapshotBeforeUpdate(prevProps, prevState) {
+//         console.log('getSnapshotBeforeUpdate');
+//         if (prevProps.color !== this.props.color) {
+//             return this.myRef.style.color;
+//         }
+//         return null;
+//     }
+
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         console.log('componentDidUpdate', prevProps, prevState);
+//         if (snapshot) {
+//             console.log('업데이트되기 직전 색상: ', snapshot);
+//         }
+//     }
+
+//     render() {
+//         console.log('render');
+
+//         const style = {
+//             color: this.props.color
+//         };
+
+//         return (
+//             <>
+//                 <h1 style={style} ref={ref => this.myRef=ref}>
+//                     {this.state.number}                    
+//                 </h1>
+//                 <p>color: {this.state.color}</p>
+//                 <button onClick={this.handleClick}>
+//                     더하기
+//                 </button>
+//             </>
+//         )
+//     }
+// }
+
+// export default LifeCycleSample;
+
+// 에러 잡아내기(의도적으로 render 함수에서 에러 발생시키기)
 import React, {Component} from 'react';
 
 class LifeCycleSample extends Component {
@@ -165,6 +247,7 @@ class LifeCycleSample extends Component {
 
         return (
             <>
+                {this.props.missing.value}
                 <h1 style={style} ref={ref => this.myRef=ref}>
                     {this.state.number}                    
                 </h1>
