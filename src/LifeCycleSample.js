c0f07ba8dc54e53c91c2c808ea84a57f46583aaf
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-11 17:13:05 +0900

    마운트 / 업데이트

--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -13,4 +13,15 @@
 // render: 작업자가 준비한 UI를 렌더링하는 메서드
 // componentDidMount: 컴포넌트가 웹 브라우저 상에 나타난 후 호출하는 메서드
 
-// 업데이트: 
+// props가 바뀔 때, state가 바뀔 때, 부모 컴포넌트가 리렌더링될 때, this.forceUpdate로 강제로 렌더링을 트리거할 때
+// 컴포넌트는 위 4가지 경우에 업데이트함
+// 업데이트시 호출하는 메서드
+// 업데이트 발생 요인 > getDerivedStateFromProps > shouldComponentUpdate > (true 반환시 render 호출, false 반환시 여기서 작업 취소)
+// > render > getSnapshotBeforeUpdate > 웹 브라우저상의 실제 DOM 변화 > componentDidUpdate
+// getDerivedStateFromProps: 마운트 과정, 업데이트 시작하기 전에 모두 호출. props 변화에 따라 state 값에도 변화주고 싶을 때 사용
+// shouldComponentUpdate: 컴포넌트가 리렌더링을 할지 말지 결정. true or false에 따라 작동 여부가 달라짐. 단, 특정 함수에서 this.forceUpdate() 함수 호출했을 때는 이 과정 생략후 바로 render 함수 호출.
+// render: 컴포넌트 리렌더링
+// getSnapshotBeforeUpdate: 컴포넌트 변화를 DOM에 반영하기 바로 직전에 호출
+// componentDidUpdate: 컴포넌트 업데이트 작업 끝난 후 호출
+
+//
