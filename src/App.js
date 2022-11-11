08c071ca2fb512d93d00a70122fd01842d4ac37e
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-11 18:02:10 +0900

    App 컴포넌트에서 예제 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -446,13 +446,47 @@
 // ====================== 5장 끝 ======================
 // ====================== 6장 시작 ======================
 // App 컴포넌트에서 예제 컴포넌트 렌더링
+// import React, {Component} from 'react';
+// import IterationSample from './IterationSample';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <IterationSample />
+//     );
+//   }
+// }
+
+// export default App;
+
+// ====================== 6장 끝 ======================
+// ====================== 7장 시작 ======================
+// App 컴포넌트에서 예제 컴포넌트 렌더링
 import React, {Component} from 'react';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSample />
+      <>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </>
     );
   }
 }
